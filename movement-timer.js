0c6adb810ad16684b0b49b85ea7b068c6bd563da
Philipp Nothvogel <phil-n@outlook.com>
2017-11-29 21:25:29 +0100

    fix: movement-timer
    works with all hours now (no leading 0 for night time).
    added working indicator.

--- movement-timer.js
+++ movement-timer.js
@@ -10,43 +10,41 @@
 var sendAt;
 var counter;
 var interval;
-
+ 
 function main() {
-	sendAt = prompt('Send attack at (hh:mm:ss):');
-	if (sendAt == null || sendAt.length != 8) {
-		return;
-	}
-	counter = -10;
-	interval = setInterval(autoTime, 100);
-	if (interval) {
-		var fragment = createElem('<h3>Movement will be sent at ' + sendAt + ' </h3><a id="workingIndicator"></a>');
-		document.getElementById('content_value').appendChild(fragment);
-	}
+    sendAt = prompt('Send attack at (hh:mm:ss):');
+    if (sendAt == null || sendAt.length > 8 || sendAt.length < 7) {
+        return;
+    }
+    counter = -10;
+    interval = setInterval(autoTime, 100);
+    if (interval) {
+        var fragment = createElem('<h3>Movement will be sent at ' + sendAt + ' </h3><a id="workingIndicator"></a>');
+        document.getElementById('content_value').appendChild(fragment);
+    }
 }
 main();
-
+ 
 function autoTime() {
-	var now = document.getElementById('serverTime').innerText;
-	if (now == sendAt) {
-		document.getElementById('troop_confirm_go').click();
-		clearInterval(interval);
-		return;
-	}
-	
-	if (document.getElementById('workingIndicator')) {
-		if (counter == 0) {
-			document.getElementById('workingIndicator').innerHTML = '.';
-		} else if (counter == 10) {
-			document.getElementById('workingIndicator').innerHTML = '..';
-		} else if (counter >= 20) {
-			document.getElementById('workingIndicator').innerHTML = '...';
-			counter = -10;
-		}
-
-		counter++;
-	}
+    var now = document.getElementById('serverTime').innerText;
+    if (now == sendAt) {
+        document.getElementById('troop_confirm_go').click();
+        clearInterval(interval);
+        return;
+    }
+    if (document.getElementById('workingIndicator')) {
+        if (counter == 0) {
+            document.getElementById('workingIndicator').innerHTML = '.';
+        } else if (counter == 10) {
+            document.getElementById('workingIndicator').innerHTML = '..';
+        } else if (counter >= 20) {
+            document.getElementById('workingIndicator').innerHTML = '...';
+            counter = -10;
+        }
+        counter++;
+    }
 }
-
+ 
 function createElem(htmlStr) {
     var frag = document.createDocumentFragment(),
         temp = document.createElement('div');
